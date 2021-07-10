1796f874ed4f65ea6b14d83e2784989ecd8db0af
kahil nayton <kahilnayton@gmail.com>
2021-07-10 17:51:44 -0400

    started stubbing out form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <h1>Hello Youtube</h1>;
+import { GetServerSideProps } from "next";
+import { getMakes, Make } from "../database/getMakes";
+import { Formik, Form } from "formik";
+import { Paper } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+
+export interface HomeProps {
+  makes: Make[];
 }
+
+export default function Home({ makes }: HomeProps) {
+  return (
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      {(values) => (
+        <Form>
+          <Paper elevation={5}>
+            <Grid container>
+              <Grid xs={12} sm={6} item>
+                MAKE
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                MODEL
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                MIN
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                MAX
+              </Grid>
+            </Grid>
+          </Paper>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const makes = await getMakes();
+  return { props: { makes } };
+};
